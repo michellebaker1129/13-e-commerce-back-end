44a7a8b02e1001ca7cf4003d1894095a5c648936
Michelle <michellebaker1129@gmail.com>
2023-05-04 18:46:47 -0700

    begin building product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,25 +4,42 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  // /api/categories/
   // find all categories
+
+  Category.findAll(
+    {
+      include: [Product]
+    }
+  ).then(
+    categories => res.json(categories)
+  )
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  // /api/categories/1
+  // req.params.id
   // find one category by its `id` value
   // be sure to include its associated Products
+  // TODO: find one category by its `id` value
 });
 
 router.post('/', (req, res) => {
+  // /api/categories/
   // create a new category
+  // TODO: create a new category
 });
 
 router.put('/:id', (req, res) => {
+  // /api/categories/1
   // update a category by its `id` value
+  // TODO: update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  // TODO: delete a category by its `id` value
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,61 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+      },
+    ],
+  })
+    .then((productData) => res.status(200).json(productData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  // TODO get one product by id
+
+  // /api/products/1
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+      },
+    ],
+  })
+  .then((productData) => {
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+    res.status(200).json(productData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+
 });
 
 // create new product
@@ -91,6 +140,8 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  // TODO delete one product by id
+  
 });
 
 module.exports = router;
